4b56de0c5dabc4263eb47d4ca6177a075326cd4a
Valentinos <poupouxios@gmail.com>
2015-11-27 22:03:29 +0000

    vagrant sites folders symlinks from the public/current folder so we dont have duplicate files

--- setup_vagrant_files.sh
+++ setup_vagrant_files.sh
@@ -9,20 +9,9 @@ if [ -d "$core_structure_folder" ]; then
 
 	items_to_symlink=( core vendor  includes misc modules profiles scripts themes authorize.php cron.php index.php update.php web.config xmlrpc.php install.php )
 
-	if [ ! -d "$vagrant_folder/sites" ]; then
-		echo "Creating $vagrant_folder/sites folder.."
-		mkdir $vagrant_folder/sites
-	fi
-
-	if [ ! -d "$vagrant_folder/sites/default" ]; then
-		echo "Creating $vagrant_folder/sites/default folder.."
-    cp -r $core_structure_folder/sites/* $vagrant_folder/sites
-    chmod 755 $vagrant_folder/sites/default
-	fi
-
-	if [ ! -h "$vagrant_folder/sites/all" ] && [ -d "$core_structure_folder/core" ]; then
-		echo "Symlinking $external_current_folder/sites/all to $vagrant_folder/sites/all"
-		ln -s $external_current_folder/sites/all $vagrant_folder/sites/all
+	if [ ! -h "$vagrant_folder/sites" ]; then
+		echo "Symlinking $external_current_folder/sites to $vagrant_folder/sites"
+		ln -s $external_current_folder/sites $vagrant_folder/sites
 	fi
 
 	for item in ${items_to_symlink[@]}; do
